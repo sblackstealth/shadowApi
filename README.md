85b1923fc621560f7fd200a8d9e69289af7cf85b
Shaedon Blackman <shaedonblackman@pursuit.org>
2019-03-21 17:39:59 -0400

    git commit -m  "finalized functionality"

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # shadowApi
+in the command line
+npm install in the command line;
+navigate to backend folder;
+navigate to db folder;
+psql -f homework.sql;
+navigate back to backend folder;
+npm start.
+
+endpoints:
+`localhost/3000/users` =list of users {[{user}{user}...]}
+`localhost/3000/users/r:?{role}` = list of users matching role specified
+`localhost/3000/users/p:?{page}`= fetches specified pagfe of list
+`localhost/3000/users/limit:?{number}`= fetches specified number of users
+`localhost/3000/users/id`:?{user_id}= fetches user with matching id
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/queries');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* -----------GET REQUEST----------- */
+router.get('/',db.getAllUsers);
+router.get('/role/:role',db.getAllUsersInRole);
+router.get('/page/:page*?',db.getAllUsersOnPage);
+router.get('/limit/:limit*?',db.getLimitedUsers);
+router.get('/id/:user_id', db.getSingleUser);
+/*------------POST REQUESt---------- */
+router.get('/addUser', db.createUser);
+/*------------PATCH  REQUESt---------- */
+router.get('/editUser/user_id', db.editUser);
 
 module.exports = router;
